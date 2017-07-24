a9335e964b887778c070b1c48440836006ee8738
Vivek Vasavada <vivek.vasavada@gmail.com>
2017-07-24 14:18:06 -0400

    added book and category models

---
+++ bookshelf2/Category.cs
@@ -0,0 +1,18 @@
+﻿using bookshelf2.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace bookshelf2
+{
+    public class Category
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<Book> Books { get; set; }
+        } 
+
+    }
---
+++ bookshelf2/Models/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace bookshelf2.Models
+{
+    public class Book
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public string Decsription { get; set; }
+        public string ISBN { get; set; }
+
+        [ForeignKey("category")]
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+        //navigation propertyj
+    }
+}
